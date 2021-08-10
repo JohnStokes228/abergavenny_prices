4998184ad5de8ec80b06b237da307ba0306df7b3
John Stokes <johnclstokes@hotmail.com>
2021-08-10 23:57:45 +0100

    added data shape table with sorting optins and scrolly thing to dashboard, couldnt figure out how to centralise the dropdown menu for some unknown reason

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ for obvious reasons...
 
 TODO:
     - add some basic data exploration graphs i.e.:
+        - basic shape info on data
         - Null count heatmap? <- filter by region prehaps to keep it legible?
         - group ranges as violin plots?
         - number of unique values in some columns?
@@ -12,22 +13,50 @@ TODO:
     - build as a multi page app with different themes and shit <- this is the big next step I think
 """
 import pandas as pd
-from typing import List
+from typing import List, Tuple, Dict
 import dash
+import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Output, Input
 import plotly.express as px
 
-
 df = pd.read_csv('data/monmouthshire_properties.csv')
+
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
     html.H1("Monmouthshire Properties Analysis", style={'text-align': 'center'}),
 
     html.Div([
-        html.H2('1. Maps of Raw Data', style={'display': 'flex', 'justifyContent': 'center', 'align-items': 'center'}),
+        html.H2('1. Variable Types and Value Counts', style={'display': 'flex',
+                                                             'justifyContent': 'center',
+                                                             'align-items': 'center'}),
+
+        html.Div([
+                dcc.Dropdown(id='sort_by',
+                             options=[{'label': 'Variable Name', 'value': 'Variable Name'},
+                                      {'label': 'Data Type', 'value': 'Data Type'},
+                                      {'label': 'Unique Values', 'value': 'Unique Values'},
+                                      {'label': 'NULL Values', 'value': 'NULL Values'},
+                                      {'label': 'Source File', 'value': 'Source File'}],
+                             value=['Variable Name'],
+                             multi=True)
+            ], style={'width': '50%', 'justifyContent': 'center', 'align-items': 'center'}),
+
+        html.Div([
+            dash_table.DataTable(id='variable_info',
+                                 fixed_rows={'headers': True},
+                                 style_table={'height': '300px', 'overflowY': 'auto'},
+                                 style_cell={'minWidth': 100}
+                                 )
+        ])
+    ]),
+
+    html.Div([
+        html.H2('2. Locations of Sold Properties', style={'display': 'flex',
+                                                          'justifyContent': 'center',
+                                                          'align-items': 'center'}),
 
         html.Div([
             dcc.Checklist(id='properties_to_plot',
@@ -61,13 +90,41 @@ app.layout = html.Div([
                                2015: {'label': '2015'},
                                2020: {'label': '2020'}}),
 
-        dcc.Markdown("""It looks like the town label is a bit sus here, especially for Crickhowell which seems to have 
-        most of its properties marked as belonging to Abergavenny. It looks like Caldicot may only be half covered by 
-        the Monmouthshire data we have too. Finally, we appear to be missing towns like Rhaglan (sick castle btw) 
-        entirely.
-        """)
-    ])
+    ]),
 ])
+
+
+@app.callback(
+    [Output(component_id='variable_info', component_property='data'),
+     Output(component_id='variable_info', component_property='columns')],
+    Input(component_id='sort_by', component_property='value')
+)
+def get_variable_info_table(sort_by: str) -> Tuple[Dict[str, str], List[Dict[str, str]]]:
+    """Generate the contents of the Dash data table displaying the shape info on the dataset.
+
+    Parameters
+    ----------
+    sort_by : Column name to sort the data on.
+
+    Returns
+    -------
+    Tuple[Dict[str, str], List[Dict[str, str]]]
+        Required outputs to fill the column headers and data content for the Dash table
+    """
+    results = pd.DataFrame(df.dtypes).reset_index()
+
+    results['Unique Values'] = [len(df[col].unique()) for col in df.columns]
+    results['NULL Values'] = [df[col].isnull().sum() for col in df.columns]
+    results['Sample Values'] = [', '.join(df[df[col].notnull()][col].unique()[:2].astype(str)) for col in df.columns]
+    results['Source File'] = 'stole it m8'  # fill this in for real at some point John its unprofessional
+
+    results.columns = ['Variable Name', 'Data Type', 'Unique Values', 'NULL Values',
+                       'Sample Values', 'Source File']
+    results['Data Type'] = results['Data Type'].astype(str)
+    results['Sample Values'] = results['Sample Values'].str[:70]
+    results.sort_values(by=sort_by, inplace=True)
+
+    return results.to_dict(orient='records'), [{'id': col, 'name': col} for col in results.columns]
 
 
 @app.callback(
@@ -100,7 +157,6 @@ def update_scatter_plot(
         x='longitude',
         y='latitude',
         color='town',
-        title='Location of properties with price data available in Monmouthshire for a given time period.'
     )
 
     return fig
