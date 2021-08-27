5408a296536f574d119a9ff0376174785e7d523d
John Stokes <johnclstokes@hotmail.com>
2021-08-27 21:43:31 +0100

    added scatter and violin plots to analyssi dahsboard, wrote some shitcode for producing voronoi shapefiles for all postcode gritties. I now wonder if ward / parish would be a better means of splitting than postcode sectors...?

--- analysis_dashboard.py
+++ analysis_dashboard.py
@@ -1,3 +1,12 @@
+"""
+Analysis dashboard, as youd expect from a file with the name of 'analysis_dashboard'. Will contain all raw
+analysis plots etc... but no weirdo models just yet.
+
+TODO: - add violins for categorical data
+      - add something amazing
+      - bask in awe at the output from the prior step
+      - legit though maybe a choropleth based on our constructed polygons or something? could be norty just sayin'
+"""
 import pandas as pd
 from typing import List
 import dash
@@ -48,7 +57,80 @@ app.layout = html.Div([
                                2020: {'label': '2020'}}),
 
     ]),
+
+    html.Div([
+        html.H2('1.2 Price vs Numeric Variables', style={'display': 'flex',
+                                                         'justifyContent': 'center',
+                                                         'align-items': 'center'}),
+
+        html.Div([
+            dcc.Dropdown(id='variables_dropdown',
+                         options=[{'label': 'new_build', 'value': 'new_build'},
+                                  {'label': 'altitude', 'value': 'altitude'},
+                                  {'label': 'supermarkets_in_area', 'value': 'supermarkets_in_area'},
+                                  {'label': 'supermarkets_in_sector', 'value': 'supermarkets_in_sector'},
+                                  {'label': 'supermarkets_in_district', 'value': 'supermarkets_in_district'},
+                                  {'label': 'distance_to_closest_supermarket', 'value': 'distance_to_closest_supermarket'},
+                                  {'label': 'number_stores_in_radius', 'value': 'number_stores_in_radius'}],
+                         value='altitude',
+                         multi=False)
+            ], style={'width': '20%', 'display': 'inline-block', 'justifyContent': 'center', 'align-items': 'center'}),
+
+        html.Div([
+            dcc.Graph(id='cost_scatter',
+                      figure={})
+        ])
+    ]),
+
+    html.Div([
+        html.H2('1.3 Price vs Categorical Variables', style={'display': 'flex',
+                                                             'justifyContent': 'center',
+                                                             'align-items': 'center'}),
+
+        dcc.Dropdown(id='variables_dropdown_2',
+                     options=[{'label': 'property_type', 'value': 'property_type'},
+                              {'label': 'estate_type', 'value': 'estate_type'},
+                              {'label': 'building_type', 'value': 'building_type'},
+                              {'label': 'town', 'value': 'town'},
+                              {'label': 'district', 'value': 'district'},
+                              {'label': 'transaction_category', 'value': 'transaction_category'},
+                              {'label': 'parish', 'value': 'parish'},
+                              {'label': 'postcode_area', 'value': 'postcode_area'},
+                              {'label': 'postcode_district', 'value': 'postcode_district'},
+                              {'label': 'postcode_sector', 'value': 'postcode_sector'},
+                              {'label': 'closest_store', 'value': 'closest_store'},
+                              {'label': 'ward', 'value': 'ward'}],
+                     value='building_type',
+                     multi=False),
+
+        html.Div([
+            dcc.Graph(id='cost_violins',
+                      figure={})
+        ])
+    ]),
 ])
+
+
+def get_requested_df(
+    properties_to_plot: List[str],
+    date_range: List[int],
+) -> pd.DataFrame:
+    """Get the requested data from the various pushy buttons bits.
+
+    Parameters
+    ----------
+    properties_to_plot : List of desired values from the 'town' column of the property dataset.
+    date_range : List of start and end date of desired date range.
+
+    Returns
+    -------
+    pd.DataFrame
+        Requested data pls.
+    """
+    requested_df = DF[DF['town'].isin(properties_to_plot)]
+    requested_df = requested_df[(requested_df['year'] >= date_range[0]) & (requested_df['year'] <= date_range[1])]
+
+    return requested_df
 
 
 @app.callback(
@@ -57,7 +139,7 @@ app.layout = html.Div([
      Input(component_id='date_range_for_price_data', component_property='value')]
 )
 def update_scatter_plot(
-    properties_to_plot: List[int],
+    properties_to_plot: List[str],
     date_range: List[int],
 ) -> px.scatter:
     """Choose which data to plot on the scatter axis.
@@ -65,15 +147,14 @@ def update_scatter_plot(
     Parameters
     ----------
     properties_to_plot : List of desired values from the 'town' column of the property dataset.
-    date_range : List of start and end date of desired date range
+    date_range : List of start and end date of desired date range.
 
     Returns
     -------
     px.scatter
         Plotly graph containing the desired points in space
     """
-    df_to_plot = DF[DF['town'].isin(properties_to_plot)]
-    df_to_plot = df_to_plot[(df_to_plot['year'] >= date_range[0]) & (df_to_plot['year'] <= date_range[1])]
+    df_to_plot = get_requested_df(properties_to_plot, date_range)
     df_to_plot = df_to_plot[df_to_plot['true_price'] == 1]
 
     fig = px.scatter(
@@ -84,6 +165,85 @@ def update_scatter_plot(
     )
 
     return fig
+
+
+@app.callback(
+    Output(component_id='cost_scatter', component_property='figure'),
+    [Input(component_id='date_range_for_price_data', component_property='value'),
+     Input(component_id='properties_to_plot', component_property='value'),
+     Input(component_id='variables_dropdown', component_property='value')]
+)
+def update_cost_scatter(
+    date_range: List[int],
+    properties_to_plot: List[str],
+    variable_to_plot: str,
+) -> px.scatter:
+    """Update contents of scatter plot which plots some scattered points for cost of property over time vs some other
+    variable chosen by a dropdown menu.
+
+    Notes
+    -----
+    Currently this includes interpolated prices, might be worth ditching these or making it possible to turn them on
+    or off prehaps?
+
+    Parameters
+    ----------
+    date_range : List of start and end date of desired date range.
+    properties_to_plot : List of towns to include in the graph.
+    variable_to_plot : Variable that will become the x axis for the resultant scatter plot.
+
+    Returns
+    -------
+    px.scatter
+        Plotly express scatter plot object displaying the analysis for the chosen period / variables
+    """
+    df_to_plot = get_requested_df(properties_to_plot, date_range)
+
+    fig = px.scatter(data_frame=df_to_plot,
+                     x=variable_to_plot,
+                     y='interpolated_price',
+                     trendline='lowess')
+
+    return fig
+
+
+@app.callback(
+    Output(component_id='cost_violins', component_property='figure'),
+    [Input(component_id='date_range_for_price_data', component_property='value'),
+     Input(component_id='properties_to_plot', component_property='value'),
+     Input(component_id='variables_dropdown_2', component_property='value')]
+)
+def update_violin_plots(
+    date_range: List[int],
+    properties_to_plot: List[str],
+    variable_to_plot: str,
+) -> px.violin:
+    """Update contents of violin plots for categorical data
+
+    Notes
+    -----
+    Currently this includes interpolated prices, might be worth ditching these or making it possible to turn them on
+    or off prehaps?
+
+    Parameters
+    ----------
+    date_range : List of start and end date of desired date range.
+    properties_to_plot : List of towns to include in the graph.
+    variable_to_plot : Variable that will become the x axis for the resultant violin plot.
+
+    Returns
+    -------
+    px.violin
+        Plotly express violin plot object displaying the analysis for the chosen period / variables
+    """
+    df_to_plot = get_requested_df(properties_to_plot, date_range)
+
+    fig = px.violin(data_frame=df_to_plot,
+                    x=variable_to_plot,
+                    y='interpolated_price')
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server()
--- construct_polygons.py
+++ construct_polygons.py
@@ -11,16 +11,57 @@ from shapely import geometry, ops
 import geopandas as gpd
 
 
-df = pd.read_csv('data/monmouthshire_properties.csv')
-df = df[['longitude', 'latitude', 'property_id',
-         'postcode_sector_longitude', 'postcode_sector_latitude', 'postcode_sector',
-         'postcode_district_longitude', 'postcode_district_latitude', 'postcode_district']].drop_duplicates()
-df.dropna(axis=0, inplace=True)
+def get_tesselation_ids(
+        longitude: str,
+        latitude: str,
+        point_set: pd.DataFrame,
+        polygons: gpd.GeoDataFrame,
+) -> gpd.GeoDataFrame:
+    """Attach meaningful IDs to a series of polygons, based on the row index of points in a dataframe which fall
+    within the polygons.
+
+    Notes
+    -----
+    I *love* sjoin thats so cool ohmygod it just knows if the points are contained within each other and joins based
+    on overlap only carries the left geometry over though incase that becomes relevant later. according to google the
+    column by name 'geometry' is the 'active geometry' of the dataframe, so if we wish to retain both geometries we
+    need only add a column equal to geometry for right df but with a different name. i.e.
+    right_df['geomtery_store'] = right_df['geometry']
+    total = gpd.tools.sjoin(left_df, right_df, how='left')
+    looks like you can use alternate rules for joins using the 'op' variable too, which defaults to = 'intersects',
+    hence the behaviour observed in this function.
+
+    Parameters
+    ----------
+    longitude : Variable name of longitude variable in point_set.
+    latitude : Variable name of latitude variable in point_set.
+    point_set : Input dataframe whose index is the desired ID variable for the polygons.
+    polygons : GeoPandas DataFrame of voronoi polygons.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        As 'polygons' input, but with the extra column 'right_index', which will contain the desired IDs.
+    """
+    point_set["geometry"] = (
+        point_set
+        .apply(lambda row: geometry.Point(row[longitude], row[latitude]), axis=1)
+    )
+    del (point_set[longitude], point_set[latitude])
+
+    point_set = gpd.GeoDataFrame(point_set, geometry='geometry')
+
+    polygons = gpd.tools.sjoin(polygons, point_set, how='left')
+    polygons.set_index('index_right', inplace=True)
+
+    return polygons
 
 
 def create_voronoi_tessellation(
     point_set: pd.DataFrame,
     file_name: str,
+    longitude: str,
+    latitude: str,
 ) -> None:
     """Create a shapefile for the resultant polygons when calculating a voronoi tessellation out of an input point set.
 
@@ -34,6 +75,8 @@ def create_voronoi_tessellation(
     ----------
     point_set : DataFrame of long / lat values for a set of unique points, whose index is point IDs.
     file_name : Name to save the shapefile under.
+    longitude : Column name of longitude variable in point_set df.
+    latitude : Column name of latitude variable in point_set df.
     """
     tessellation = Voronoi(point_set)
     edges = [geometry.LineString(tessellation.vertices[line])
@@ -41,33 +84,31 @@ def create_voronoi_tessellation(
              if -1 not in line]  # taken from a helpful stack overflow comment
 
     polygons = [gpd.GeoSeries(poly) for poly in ops.polygonize(edges)]
-    polygons = gpd.GeoSeries(pd.concat(polygons))
+    polygons = gpd.GeoDataFrame(pd.concat(polygons))
+    polygons.columns = ['geometry']
 
+    polygons = get_tesselation_ids(longitude=longitude,
+                                   latitude=latitude,
+                                   point_set=point_set,
+                                   polygons=polygons)
     polygons.to_file(f'data/polygons/{file_name}.shp',
                      driver='ESRI Shapefile',
                      index=False)
 
 
-### shit code just for testing ###
-"""
-Looking for a way to carry ID across from the input data to the resultant geopandas frame.
-"""
-point_set = df[['postcode_sector_longitude', 'postcode_sector_latitude', 'postcode_sector']].drop_duplicates()
-point_set.set_index('postcode_sector', inplace=True)
-tessellation = Voronoi(point_set)
-edges = [geometry.LineString(tessellation.vertices[line])
-         for line in tessellation.ridge_vertices
-         if -1 not in line]
+df = pd.read_csv('data/monmouthshire_properties.csv')
+df = df[['longitude', 'latitude', 'property_id',
+         'postcode_sector_longitude', 'postcode_sector_latitude', 'postcode_sector',
+         'postcode_district_longitude', 'postcode_district_latitude', 'postcode_district']].drop_duplicates()
+df.dropna(axis=0, inplace=True)
+test = df[['longitude', 'latitude', 'property_id']].drop_duplicates()
+test.set_index('property_id', inplace=True)
+create_voronoi_tessellation(test, 'postcode_polygons', 'longitude', 'latitude')
 
-polygons = [gpd.GeoSeries(poly) for poly in ops.polygonize(edges)]
-polygons = gpd.GeoDataFrame(pd.concat(polygons))
-polygons.columns = ['geometry']
+test = df[['postcode_sector_longitude', 'postcode_sector_latitude', 'postcode_sector']].drop_duplicates()
+test.set_index('postcode_sector', inplace=True)
+create_voronoi_tessellation(test, 'postcode_sector_polygons', 'postcode_sector_longitude', 'postcode_sector_latitude')
 
-point_set["geometry"] = point_set.apply(lambda row: geometry.Point(row["postcode_sector_longitude"], row["postcode_sector_latitude"]), axis=1)
-del(point_set['postcode_sector_longitude'], point_set['postcode_sector_latitude'])
-point_set = gpd.GeoDataFrame(point_set, geometry='geometry')
-test = gpd.tools.sjoin(polygons, point_set, how='left')
-test.set_index('index_right', inplace=True)
-# I *love* sjoin thats so cool ohmygod
-# it just knows if the points are contained within each other and joins based on overlap
-# only carries the left geometry over though incase that becomes relevant later
+test = df[['postcode_district_longitude', 'postcode_district_latitude', 'postcode_district']].drop_duplicates()
+test.set_index('postcode_district', inplace=True)
+create_voronoi_tessellation(test, 'postcode_district_polygons', 'postcode_district_longitude', 'postcode_district_latitude')
